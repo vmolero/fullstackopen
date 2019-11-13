9548f955cefdd0caf87898babf659468dd11da0a
Victor Molero <victor.molero@kuveno.com>
2019-11-13 12:19:35 +0100

    refactor step 1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,28 +4,46 @@ import ReactDOM from "react-dom";
 const Header = props => {
   return <h1>{props.course}</h1>;
 };
+
+const Total = props => {
+  return <p>Number of exercises {props.exercises}</p>;
+};
+
+const Content = props => {
+  return (
+    <>
+      <p>
+        {props.parts[0].part} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].part} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].part} {props.parts[2].exercises}
+      </p>
+    </>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
   const exercises1 = 10;
-  const part2 = "Using props to pass data";
   const exercises2 = 7;
-  const part3 = "State of a component";
   const exercises3 = 14;
+  const part1 = "Fundamentals of React";
+  const part2 = "Using props to pass data";
+  const part3 = "State of a component";
+  const parts = [
+    { part: part1, exercises: exercises1 },
+    { part: part2, exercises: exercises2 },
+    { part: part3, exercises: exercises3 }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content parts={parts} />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
