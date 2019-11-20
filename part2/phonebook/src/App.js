1b826b598b5811b32bd964f81158d5465c8f811f
Victor Molero <victor.molero@kuveno.com>
2019-11-20 13:41:19 +0100

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Arto Hellas", number: "040-123456" },
+    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const handleNameChange = event => {
     setNewName(event.target.value);
@@ -11,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = event => {
     setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = event => {
+    setNewFilter(event.target.value);
   };
 
   const addPerson = event => {
@@ -23,7 +33,7 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length
+      id: persons.length + 1
     };
 
     setPersons(persons.concat(personObject));
@@ -31,17 +41,27 @@ const App = () => {
     setNewNumber("");
   };
 
-  const listing = persons.map(person => {
-    return (
-      <li key="person.id">
-        {person.name} {person.number}
-      </li>
-    );
-  });
+  const listing = persons
+    .filter(person => {
+      return person.name.toLowerCase().indexOf(newFilter.toLowerCase()) > -1;
+    })
+    .map(person => {
+      return (
+        <li key={person.id}>
+          {person.name} {person.number}
+        </li>
+      );
+    });
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <h2>Search</h2>
+      <div>
+        By name:
+        <input type="text" value={newFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:
