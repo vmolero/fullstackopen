f496816346fabaae2cdace4a2d1827c6647bd315
Victor Molero <victor.molero@kuveno.com>
2019-11-27 22:30:58 +0100

    phonebook step8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -33,7 +33,7 @@ const App = () => {
       name: newName,
       number: newNumber
     };
-    axios.post("http://localhost:3001/persons", personObject).then(() => {
+    personService.create(personObject).then(() => {
       setPersons(persons.concat(personObject));
       setNewName("");
       setNewNumber("");
@@ -41,7 +41,7 @@ const App = () => {
   };
 
   const hook = () => {
-    axios.get("http://localhost:3001/persons").then(response => {
+    personService.getAll().then(response => {
       setPersons(response.data);
     });
   };
